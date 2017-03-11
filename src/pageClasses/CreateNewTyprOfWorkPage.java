0e50e140ea2f610160dae66f09883354feeb3ac5
avinashbinu <011.avinash@gmail.com>
2017-03-11 19:52:02 +0530

    Update CreateNewTyprOfWorkPage.java
    ThirdPush

--- src/pageClasses/CreateNewTyprOfWorkPage.java
+++ src/pageClasses/CreateNewTyprOfWorkPage.java
@@ -20,7 +20,7 @@ public class CreateNewTyprOfWorkPage {
 	
 	public void creatingNewWork()
 	{
-		nameTextBox.sendKeys("Testing22");
+		nameTextBox.sendKeys("Testing23");
 		createTypeofWorkButton.click();
 	}
 }
